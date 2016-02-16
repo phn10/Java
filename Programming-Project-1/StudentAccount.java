40db6c04444be1ae4996332a80569dfb87f520f4
phn10 <phn10@users.noreply.github.com>
2016-02-15 21:35:02 -0500

    Update StudentAccount.java

--- Programming-Project-1/StudentAccount.java
+++ Programming-Project-1/StudentAccount.java
@@ -1,13 +1,12 @@
 public class StudentAccount extends Account
 {
   private String name = " ";        // store student name variable
-  private String address = " ";     // store student address variable
-  private double balance = 0;       // store student balance
+  private String address = " ";     // store student address variable   
   private double refund = 0;        // store the refund amount
    
-  LibraryAccount libraryAccount;    // library account
-  CreditAccount tuitionAccount;     // tuition account 
-  CreditAccount diningAccount;      // dining account
+  LibraryAccount libraryAccount = null;    // library account
+  CreditAccount tuitionAccount = null;     // tuition account 
+  CreditAccount diningAccount = null;      // dining account
   
   // class constructor set student account and student name
   public StudentAccount(String account, String name)
@@ -79,18 +78,52 @@ public class StudentAccount extends Account
   // return the student's account balance after the refund
   public double getBalance()
   {
-    balance = libraryAccount.getBalance() + 
-              tuitionAccount.getBalance() + 
-              diningAccount.getBalance();
-    return balance - refund;
+    double balance = 0;
+    
+    if (tuitionAccount != null && refund > 0)
+    {
+      if (refund > tuitionAccount.getBalance())
+      {
+        refund -= tuitionAccount.getBalance();
+        tuitionAccount.credit(tuitionAccount.getBalance());
+      }
+      else
+      {
+        tuitionAccount.credit(refund);
+        refund = 0;
+      }
+    }
+    
+    if (diningAccount != null && refund > 0)
+    {
+      if (refund > diningAccount.getBalance())
+      {
+        refund -= diningAccount.getBalance();
+        tuitionAccount.credit(diningAccount.getBalance());
+      }
+      else
+      {
+        diningAccount.credit(refund);
+        refund = 0;
+      }
+    }
+   
+    if (libraryAccount != null)
+      balance += libraryAccount.getBalance();
+    if (tuitionAccount != null)
+      balance += tuitionAccount.getBalance();
+    if (diningAccount != null)
+      balance += diningAccount.getBalance();
+    
+    return balance;
   }
   
   // charge the student account
-  // if the charge amount is less than refund, the refund will be negate by the difference
+  // if the charge amount is less than refund, the refund will be negated by the difference
   // else if the charge is more than refund, the account balance will be increased by the difference
   public void charge(double charge)
   {
-    if (this.refund < charge)
+    if (this.refund < charge && tuitionAccount != null)
     {
       charge -= this.refund;
       this.refund = 0;
@@ -101,34 +134,55 @@ public class StudentAccount extends Account
       this.refund -= charge;
     }
   }
-    
+  
+  // subtract the student account balance in tuition account, dining account and library account order
   public void credit(double credit)
   {
-    if (credit > tuitionAccount.getBalance())
+    if (tuitionAccount != null && credit > 0)
     {
-      credit -= tuitionAccount.getBalance();
-      tuitionAccount.credit(tuitionAccount.getBalance());
-        
-      if (credit > diningAccount.getBalance())
+      if (credit > tuitionAccount.getMonthlyPayment())
       {
-        credit -= diningAccount.getBalance();
-        diningAccount.credit(diningAccount.getBalance());
-          
-        if (credit > libraryAccount.getBalance())
-        {
-           credit -= libraryAccount.getBalance();
-           libraryAccount.credit(libraryAccount.getBalance());
-           refund = credit;
-          }
-           else
-           {
-             libraryAccount.credit(credit);
-           }
-          }
-        else
-        {
-          diningAccount.credit(credit);
-        } 
+        credit -= tuitionAccount.getMonthlyPayment();
+        tuitionAccount.credit(tuitionAccount.getMonthlyPayment());
+      }
+      else
+      {
+        tuitionAccount.credit(credit);
+        credit = 0;
+      }
+    }
+    
+    if (diningAccount != null && credit > 0)
+    {
+      if (credit > diningAccount.getMonthlyPayment())
+      {
+        credit -= diningAccount.getMonthlyPayment();
+        diningAccount.credit(diningAccount.getMonthlyPayment());
+      }
+      else
+      {
+        diningAccount.credit(credit);
+        credit = 0;
+      }
+    }
+    
+    if (libraryAccount != null  && credit > 0)
+    {   
+      if (credit > libraryAccount.getBalance())
+      {
+        credit -= libraryAccount.getBalance();
+        libraryAccount.credit(libraryAccount.getBalance());
+      }
+      else
+      {
+        libraryAccount.credit(libraryAccount.getBalance());
+        credit = 0;
+      }
+    }
+    
+    refund = credit;
+  }
+}
       }
       else
       {

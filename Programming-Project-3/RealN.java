c4d038f1e90715cf84d5c26430e75c78017ccee6
phn10 <phn10@users.noreply.github.com>
2016-03-21 17:13:44 -0400

    Update RealN.java

--- Programming-Project-3/RealN.java
+++ Programming-Project-3/RealN.java
@@ -1,5 +1,13 @@
+/**
+ * This class represents the real number
+ * @author Phong Nguyen
+ */
 public class RealN extends ComplexN
 { 
+  /**
+   * Constructor
+   * @param realNum the real number
+   */
   public RealN(double realNum)
   {
     super(realNum, 0);

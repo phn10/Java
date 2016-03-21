c84813f06d1f3392d76333a756384cd4ebc3dd02
phn10 <phn10@users.noreply.github.com>
2016-03-21 17:13:16 -0400

    Update RationalN.java

--- Programming-Project-3/RationalN.java
+++ Programming-Project-3/RationalN.java
@@ -1,9 +1,18 @@
+/**
+ * This class represents a rational number.
+ * @author Phong Nguyen
+ */
 public class RationalN extends RealN 
 {
-  private int num;
-  private int den;
+  private int num;          // the numerator of the rational number
+  private int den;          // the denominator of the rational number
   
-  // constructor
+  /**
+   * Constructor 
+   * @param num the numerator of the rational number
+   * @param den the denoninator of the rational number
+   * @throws ArithmeticException if the denominator equals to 0
+   */
   public RationalN(int num, int den)
   {
     super((double)num / (double)den);
@@ -18,18 +27,29 @@ public class RationalN extends RealN
     }
   }
   
+   /**
+   * Get the value of the numerator
+   */
   public int getNumerator()
   {
     return num;
   }
   
+  /**
+   * Get the value of the denominator
+   */
   public int getDenominator()
   {
     return den;
   }
   
-  public static int gcd(int a, int b)
+  /**
+   * Get the greatest common divisor between the numerator and denominator
+   */
+  public int gcd()
   {
+    int a = this.getNumerator();         // numerator of the rational number
+    int b = this.getDenominator();       // denominator of the rational number
     while (a % b != 0)
     {
       int temp = a;
@@ -39,7 +59,10 @@ public class RationalN extends RealN
     return b;
   }
   
-  public String toString()
+  /**
+   * Return the string representation of the rational number
+   */
+  public String toString() 
   {
     if (this.getNumerator() == 0)
     {
@@ -47,16 +70,14 @@ public class RationalN extends RealN
     }
     else
     {
-      if (this.getDenominator() == 1 || this.getDenominator() / this.gcd(this.getNumerator(), this.getDenominator()) == 1)
-        return Integer.toString(this.getNumerator() / this.gcd(this.getNumerator(), this.getDenominator()));
+      if (this.getDenominator() == 1 || this.getDenominator() / this.gcd() == 1)
+        return Integer.toString(this.getNumerator() / this.gcd());
       else
       {
         if (this.getNumerator() * (double)this.getDenominator() >= 0)
-          return Math.abs(this.getNumerator() / this.gcd(this.getNumerator(), this.getDenominator())) + "/" + 
-          Math.abs(this.getDenominator() / this.gcd(this.getNumerator(), this.getDenominator()));
+          return Math.abs(this.getNumerator() / this.gcd()) + "/" + Math.abs(this.getDenominator() / this.gcd());
         else
-          return "-" + Math.abs(this.getNumerator() / this.gcd(this.getNumerator(), this.getDenominator())) + "/" + 
-          Math.abs(this.getDenominator() / this.gcd(this.getNumerator(), this.getDenominator()));
+          return "-" + Math.abs(this.getNumerator() / this.gcd()) + "/" + Math.abs(this.getDenominator() / this.gcd());
       }
     }
   }

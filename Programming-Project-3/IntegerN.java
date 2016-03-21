97603ce66ab7b01efe3699499f15de2bf92c5b8c
phn10 <phn10@users.noreply.github.com>
2016-03-21 17:11:44 -0400

    Update IntegerN.java

--- Programming-Project-3/IntegerN.java
+++ Programming-Project-3/IntegerN.java
@@ -1,5 +1,13 @@
+/**
+ * This class represents integer number
+ * @author Phong Nguyen
+ */
 public class IntegerN extends RationalN
 {
+ /**
+   * Constructor
+   * @param num integer number
+   */
  public IntegerN(int num)
  {
    super(num, 1);

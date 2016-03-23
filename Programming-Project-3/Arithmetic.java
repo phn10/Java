ff1a3146c66b84ee6138d1cc41dff801d51d1ed0
phn10 <phn10@users.noreply.github.com>
2016-03-23 19:33:19 -0400

    Update Arithmetic.java

--- Programming-Project-3/Arithmetic.java
+++ Programming-Project-3/Arithmetic.java
@@ -4,169 +4,303 @@
  */
 public class Arithmetic
 {
-  private static ComplexN num;   // the result of each methods
-  private static double real;    // the real part of the number
-  private static double img;     // the imaginary part of the number
+  /** FIND SUM **\
+   
+  /**
+   * Find the sum of two natural number 
+   * @param num1 the first natural number
+   * @param num2 the second natural number
+   */ 
+  public static NaturalN add(NaturalN num1, NaturalN num2)
+  {
+    int num = num1.getNumerator() + num2.getNumerator();
+    NaturalN nat = new NaturalN(num);
+    return  nat;
+  }
   
   /**
-   * Find the sum of two number 
-   * @param num1 the first number
-   * @param num2 the second number
-   */
-  public static ComplexN add(ComplexN num1, ComplexN num2)
+   * Find the sum of two integer number 
+   * @param num1 the first integer number
+   * @param num2 the second integer number
+   */ 
+  public static IntegerN add(IntegerN num1, IntegerN num2)
   {
-    real = num1.getRealPart() + num2.getRealPart();
-    img = num1.getImaginaryPart() + num2.getImaginaryPart();
-    
-    if (num1 instanceof NaturalN && num2 instanceof NaturalN)
-      num = new NaturalN((int) real);
-    else if (num1 instanceof IntegerN && num2 instanceof IntegerN)
-      num = new IntegerN((int)real);
-    else if (num1 instanceof RationalN && num2 instanceof RationalN)
-    {
-      RationalN rat1;          // first rational number
-      RationalN rat2;          // second rational number
-      int numerator;           // numerator of the sum of two rational numbers
-      int denominator;         // denominator of the sum of two rational numbers
+    int num3 = num1.getNumerator() + num2.getNumerator();
+    IntegerN num = new IntegerN(num3);
+    return  num;
+  }
+  
+  /**
+   * Find the sum of two rational number 
+   * @param num1 the first rational number
+   * @param num2 the second rational number
+   */ 
+  public static RationalN add(RationalN num1, RationalN num2)
+  {
+    int numerator;           // numerator of the sum of two rational numbers
+    int denominator;         // denominator of the sum of two rational numbers
       
-      rat1 = (RationalN)num1;  // type cast the first input as rational number
-      rat2 = (RationalN)num2;  // type cast the second input as rational number
+    numerator = (num1.getNumerator() * num2.getDenominator() + num2.getNumerator() * num1.getDenominator());
+    denominator = num1.getDenominator() * num2.getDenominator();
       
-      numerator = (int)(rat1.getNumerator() * rat2.getDenominator() + rat2.getNumerator() * rat1.getDenominator());
-      denominator = (int)(rat1.getDenominator() * rat2.getDenominator());
-      
-      num = new RationalN(numerator, denominator);
-    }
-    else if (num1 instanceof RealN && num2 instanceof RealN)
-      num = new RealN(real);
-    else if (num1 instanceof ComplexN && num2 instanceof ComplexN)
-      num = new ComplexN(real, img);
-    
+    RationalN num = new RationalN(numerator, denominator);
     return num;
   }
   
   /**
-   * Find the result when subtract the first number by the second number
-   * @param num1 the first number
-   * @param num2 the second number
-   * @throws ArithmeticException if both number are NaturalN type and the second number is greater than the first number
+   * Find the sum of two real number 
+   * @param num1 the first real number
+   * @param num2 the second real number
+   */ 
+  public static RealN add(RealN num1, RealN num2)
+  {
+    double num3 = num1.getRealPart() + num2.getRealPart();
+    RealN num = new RealN(num3);
+    return num;
+  }
+  
+  /**
+   * Find the sum of two complex number 
+   * @param num1 the first complex number
+   * @param num2 the second complex number
+   */ 
+  public static ComplexN add(ComplexN num1, ComplexN num2)
+  {
+    double real = num1.getRealPart() + num2.getRealPart();
+    double img = num1.getImaginaryPart() + num2.getImaginaryPart();
+    
+    ComplexN num = new ComplexN(real, img);
+    return num;
+  }
+  
+  
+  /** FIND SUBTRACTION **\
+ 
+  /*
+   * Find the subtraction of two natural numbers
+   * @param num1 the first natural number
+   * @param num2 the second natural number
+   * @throws ArithmeticException if the second number is greater than the first number
+   */
+  public static NaturalN subtract(NaturalN num1, NaturalN num2)
+  {
+    int num3;
+    if(num1.getNumerator() < num2.getNumerator())
+      throw new ArithmeticException("Cannot subtract bigger number from smaller number");
+    else
+      num3 = num1.getNumerator() - num2.getNumerator();
+    
+    NaturalN num = new NaturalN(num3);
+    return num;
+  }
+  
+ /*
+   * Find the subtraction of two integer numbers
+   * @param num1 the first integer number
+   * @param num2 the second integer number
+   */
+  public static IntegerN subtract(IntegerN num1, IntegerN num2)
+  {
+    int num3 = num1.getNumerator() - num2.getNumerator(); 
+    NaturalN num = new NaturalN(num3);
+    return num;
+  }
+      
+  /*
+   * Find the subtraction of two rational numbers
+   * @param num1 the first rational number
+   * @param num2 the second rational number
+   */
+  public static RationalN subtract(RationalN num1, RationalN num2)
+  {
+    int numerator;           // numerator of the sum of two rational numbers
+    int denominator;         // denominator of the sum of two rational numbers
+      
+    numerator = (num1.getNumerator() * num2.getDenominator() - num2.getNumerator() * num1.getDenominator());
+    denominator = num1.getDenominator() * num2.getDenominator();
+      
+    RationalN num = new RationalN(numerator, denominator);
+    return num;
+  }
+  
+  /*
+   * Find the subtraction of two real numbers
+   * @param num1 the first real number
+   * @param num2 the second real number
+   */
+  public static RealN subtract(RealN num1, RealN num2)
+  {
+    double num3 = num1.getRealPart() - num2.getRealPart(); 
+    RealN num = new RealN(num3);
+    return num;
+  }
+  
+  /*
+   * Find the subtraction of two complex numbers
+   * @param num1 the first complex number
+   * @param num2 the second complex number
    */
   public static ComplexN subtract(ComplexN num1, ComplexN num2)
   {
-    real = num1.getRealPart() - num2.getRealPart();
-    img = num1.getImaginaryPart() - num2.getImaginaryPart();
+    double real = num1.getRealPart() - num2.getRealPart();
+    double img = num1.getImaginaryPart() - num2.getImaginaryPart();
     
-    if (num1 instanceof NaturalN && num2 instanceof NaturalN)
-    {
-      if (num1.getRealPart() < num2.getRealPart())
-        throw new ArithmeticException("Cannot subtract smaller number from a larger one");
-      else
-        num = new NaturalN((int) real);
-    }
-    else if (num1 instanceof IntegerN && num2 instanceof IntegerN)
-      num = new IntegerN((int)real);
-    else if (num1 instanceof RationalN && num2 instanceof RationalN)
-    {
-      RationalN rat1;            // first rational number
-      RationalN rat2;            // second rational number
-      int numerator;             // numerator of the sum of two rational numbers
-      int denominator;           // denominator of the sum of two rational numbers
-      
-      rat1 = (RationalN)num1;    // type cast the first input as rational number
-      rat2 = (RationalN)num2;    // type cast the second input as rational number
-      
-      numerator = (int)(rat1.getNumerator() * rat2.getDenominator() - rat2.getNumerator() * rat1.getDenominator());
-      denominator = (int)(rat1.getDenominator() * rat2.getDenominator());
-      
-      num = new RationalN(numerator, denominator);
-    }
-    else if (num1 instanceof RealN && num2 instanceof RealN)
-      num = new RealN(real);
-    else if (num1 instanceof ComplexN && num2 instanceof ComplexN)
-      num = new ComplexN(real, img);
-    
+    ComplexN num = new ComplexN(real, img);
     return num;
   }
   
-  /**
-   * Find the result when multiply the first number by the second number
-   * @param num1 the first number
-   * @param num2 the second number
+  
+  /** FIND MULTIPLICATION **\
+  /*
+   * Find the multiplication of two natural numbers
+   * @param num1 the first natural number
+   * @param num2 the second natural number
+   */
+  public static NaturalN multiply(NaturalN num1, NaturalN num2)
+  {
+    int num3 = num1.getNumerator() * num2.getNumerator();
+    NaturalN num = new NaturalN(num3);
+    return num;
+  }
+  
+  /*
+   * Find the multiplication of two integer numbres
+   * @param num1 the first integer number
+   * @param num2 the second integer number
+   */
+  public static IntegerN multiply(IntegerN num1, IntegerN num2)
+  {
+    int num3 = num1.getNumerator() * num2.getNumerator();
+    IntegerN num = new IntegerN(num3);
+    return num;
+  }
+  
+  /*
+   * Find the multiplication of two rational numbers
+   * @param num1 the first rational number
+   * @param num2 the second rational number
+   */
+  public static RationalN multiply(RationalN num1, RationalN num2)
+  {
+    int numerator = num1.getNumerator() * num2.getNumerator();
+    int denominator = num1.getDenominator() * num2.getDenominator();
+    
+    RationalN num = new RationalN(numerator, denominator);
+    return num;
+  }
+  
+  /*
+   * Find the multiplication of two real numbres
+   * @param num1 the first real number
+   * @param num2 the second real number
+   */
+  public static RealN multiply(RealN num1, RealN num2)
+  {
+    double num3 = num1.getRealPart() * num2.getRealPart();
+    RealN num = new RealN(num3);
+    return num;
+  }
+  
+  /*
+   * Find the multiplication of two complex numbers
+   * @param num1 the complex number
+   * @param num2 the complex number
    */
   public static ComplexN multiply(ComplexN num1, ComplexN num2)
   {
-    real = num1.getRealPart() * num2.getRealPart() - num1.getImaginaryPart() * num2.getImaginaryPart();
-    img = num1.getRealPart() * num2.getImaginaryPart() + num1.getImaginaryPart() * num2.getRealPart();
- 
-    if (num1 instanceof NaturalN && num2 instanceof NaturalN)
-      num = new NaturalN((int) real);
-    else if (num1 instanceof IntegerN && num2 instanceof IntegerN)
-      num = new IntegerN((int)real);
-    else if (num1 instanceof RationalN && num2 instanceof RationalN)
-    {
-      RationalN rat1;             // first rational number
-      RationalN rat2;             // second rational number
-      int numerator;              // numerator of the sum of two rational numbers
-      int denominator;            // denominator of the sum of two rational numbers
+    double real = num1.getRealPart() * num2.getRealPart() - num1.getImaginaryPart() * num2.getImaginaryPart();
+    double img = num1.getRealPart() * num2.getImaginaryPart() + num1.getImaginaryPart() * num2.getRealPart();
       
-      rat1 = (RationalN)num1;     // type cast the first input as rational number
-      rat2 = (RationalN)num2;     // type cast the second input as rational number
-      
-      numerator = rat1.getNumerator() * rat2.getNumerator();
-      denominator = rat1.getDenominator() * rat2.getDenominator();
-      
-      num = new RationalN(numerator, denominator);
-    }
-    else if (num1 instanceof RealN && num2 instanceof RealN)
-      num = new RealN(real);
-    else if (num1 instanceof ComplexN && num2 instanceof ComplexN)
-      num = new ComplexN(real, img);
-    
+    ComplexN num = new ComplexN(real, img);
     return num;
   }
   
-  /**
-   * Find the result when divide the first number by the second number
-   * @param num1 the first number
-   * @param num2 the second number
-   * @throws ArithmeticException if the divisor equals to 0
+  
+  /** FIND DIVISION **\
+  
+  /*
+   * Find division of two natural numbers
+   * @param num1 the first natural number
+   * @param num2 the second natural number
+   * @throws ArithmeticException if the second number, divisor, equals to 0
+   */
+  public static NaturalN divide(NaturalN num1, NaturalN num2)
+  {
+    int num3 = num1.getNumerator() / num2.getNumerator();
+    if (num2.getNumerator() == 0)
+      throw new ArithmeticException("Cannot divide a number by 0");
+    
+    NaturalN num = new NaturalN(num3);
+    return num;
+  }
+  
+  /*
+   * Find division of two integer numbers
+   * @param num1 the first integer number
+   * @param num2 the second integer number
+   * @throws ArithmeticException if the second number, divisor, equals to 0
+   */
+  public static IntegerN divide(IntegerN num1, IntegerN num2)
+  {
+    int num3 = num1.getNumerator() / num2.getNumerator();
+    if (num2.getNumerator() == 0)
+      throw new ArithmeticException("Cannot divide a number by 0");
+    
+    IntegerN num = new IntegerN(num3);
+    return num;
+  }
+  
+  /*
+   * Find division of two rational numbers
+   * @param num1 the first rational number
+   * @param num2 the second rational number
+   * @throws ArithmeticException if the second number, divisor, equals to 0
+   */
+  public static RationalN divide(RationalN num1, RationalN num2)
+  {
+    int numerator = num1.getNumerator() * num2.getDenominator();
+    int denominator = num1.getDenominator() * num2.getNumerator();
+    
+    if (num2.getNumerator() == 0)
+      throw new ArithmeticException("Cannot divide a number by 0");
+    
+    RationalN num = new RationalN(numerator, denominator);
+    return num;
+  }
+  
+  /*
+   * Find division of two real numbers
+   * @param num1 the first real number
+   * @param num2 the second real number
+   * @throws ArithmeticException if the second number, divisor, equals to 0
+   */
+  public static RealN divide(RealN num1, RealN num2)
+  {
+    double num3 = num1.getRealPart() / num2.getRealPart();
+    if (num2.getRealPart() == 0)
+      throw new ArithmeticException("Cannot divide a number by 0");
+    
+    RealN num = new RealN(num3);
+    return num;
+  }
+  
+  /*
+   * Find the division of two complex numbers
+   * @param num1 the first complex number
+   * @param num2 the second complex number
+   * @throws ArithmeticException if the second number, divisor, equals to 0
    */
   public static ComplexN divide(ComplexN num1, ComplexN num2)
   {
+    double real = (num1.getRealPart() * num2.getRealPart() + num1.getImaginaryPart() * num2.getImaginaryPart()) / 
+        (Math.pow(num2.getRealPart(), 2) + Math.pow(num2.getImaginaryPart(), 2));
+    double img = -(num1.getRealPart() * num2.getImaginaryPart()) + num1.getImaginaryPart() * num2.getRealPart() / 
+        (Math.pow(num2.getRealPart(), 2) + Math.pow(num2.getImaginaryPart(), 2));
+    
     if (num2.getRealPart() == 0 && num2.getImaginaryPart() == 0)
       throw new ArithmeticException("Cannot divide a number by 0");
-    else
-    {
-      real = (num1.getRealPart() * num2.getRealPart() + num1.getImaginaryPart() * num2.getImaginaryPart()) / 
-        (Math.pow(num2.getRealPart(), 2) + Math.pow(num2.getImaginaryPart(), 2));
-      img = -(num1.getRealPart() * num2.getImaginaryPart()) + num1.getImaginaryPart() * num2.getRealPart() / 
-        (Math.pow(num2.getRealPart(), 2) + Math.pow(num2.getImaginaryPart(), 2));
-      
-      if (num1 instanceof NaturalN && num2 instanceof NaturalN)
-        num = new NaturalN((int) real);
-      else if (num1 instanceof IntegerN && num2 instanceof IntegerN)
-        num = new IntegerN((int)real);
-      else if (num1 instanceof RationalN && num2 instanceof RationalN)
-      {
-        RationalN rat1;             // first rational number
-        RationalN rat2;             // second rational number
-        int numerator;              // numerator of the sum of two rational numbers
-        int denominator;            // denominator of the sum of two rational numbers
-      
-        rat1 = (RationalN)num1;     // type cast the first input as rational number
-        rat2 = (RationalN)num2;     // type cast the second input as rational number
         
-        numerator = rat1.getNumerator() * rat2.getDenominator();
-        denominator = rat1.getDenominator() * rat2.getNumerator();
-   
-        num = new RationalN(numerator, denominator);
-      }
-      else if (num1 instanceof RealN && num2 instanceof RealN)
-        num = new RealN(real);
-      else if (num1 instanceof ComplexN && num2 instanceof ComplexN)
-        num = new ComplexN(real, img);
-    }
-    
+    ComplexN num = new ComplexN(real, img);
     return num;
   }
 }

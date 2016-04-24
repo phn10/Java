54bd8569b1e90210873882000bc0ab25f433afbe
phn10 <phn.sci@gmail.com>
2016-04-24 11:20:29 -0400

    update 2

---
+++ Project5/DNATester.java
@@ -0,0 +1,17 @@
+/**
+ * Name: Phong Nguyen
+ * ID  : phn10
+ */
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+/**
+ * A class that tests the methods of the DNA class
+ * @author Phong Nguyen
+ */
+public class DNATester 
+{
+}
